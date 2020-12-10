ccd0d3cb4f390eab8d6b9367015ac206ab3bd34b
minha62 <71062967+minha62@users.noreply.github.com>
2020-12-11 04:48:07 +0900

    Update Router.js

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,16 +1,42 @@
 import React, { useState } from "react";
 import {HashRouter as Router, Route, Switch} from "react-router-dom";
 import Auth from "routes/Auth";
-import Home from "routes/Home";
+import Search from "routes/Search";
+import Release from"routes/Release";
+import Roommates from "routes/Roommates";
+import Community from "routes/Community";
+import Notice from "routes/Notice";
+import MyProfile from "routes/MyProfile";
+import LogOut from "routes/LogOut";
+import Navigation from "components/Navigation";
 
 const AppRouter = ({isLoggedIn}) => {
     return (
         <Router>
+            {isLoggedIn && <Navigation />}
             <Switch>
                 {isLoggedIn ? (
                     <>
                     <Route exact path = "/">
-                        <Home />
+                        <Search />
+                    </Route> 
+                    <Route exact path = "/Release">
+                        <Release />
+                    </Route> 
+                    <Route exact path = "/Roommates">
+                        <Roommates />
+                    </Route> 
+                    <Route exact path = "/Community">
+                        <Community />
+                    </Route> 
+                    <Route exact path = "/Notice">
+                        <Notice />
+                    </Route> 
+                    <Route exact path = "/MyProfile">
+                        <MyProfile />
+                    </Route> 
+                    <Route exact path = "/LogOut">
+                        <LogOut />
                     </Route> 
                     </> 
                 ) : (
