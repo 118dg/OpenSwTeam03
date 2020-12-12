e0918562b6fc8523a56b4715fedafdb838cbdba2
minha62 <71062967+minha62@users.noreply.github.com>
2020-12-13 07:00:57 +0900

    Update Release.js

--- src/routes/Release.js
+++ src/routes/Release.js
@@ -2,50 +2,56 @@ import { dbService } from "fbase";
 import React, { useEffect, useState } from "react";
 
 const Release = () => {
-    const [release, setRelease] = useState("");
-    const [releases, setReleases] = useState([]);
-    const getReleases = async() => {
-        const dbReleases = await dbService.collection("releases").get();
-        dbReleases.forEach((document) => {
-            const releaseObject = {
-                ...document.data(),
-                id: document.id,
-            };
-            setReleases((prev) => [releaseObject, ...prev]);
-        });
-    };
-    useEffect(() => {
-        getReleases();
-    }, []);
-    const onSubmit = async (event) => {
-        event.preventDefault();
-        await dbService.collection("release").add({
-            release,
-            createdAt: Date.now()
-        });
-        setRelease("");
-    };
-const onChange = (event)=>{
-    const {target:{value}} = event;
-    setRelease(value);
-};
-
-console.log(releases);
-
-    return(
-        <div>
-            <form onSubmit={onSubmit}>
-                <input value={release} onChange={onChange} type="text" placeholder="리뷰를 남겨주세요!" maxLength={300} />
-                <input type="submit" value="저장" />
-            </form>
-            <div>
-                {releases.map((release) => (
-                    <div key={release.id}>
-                        <h4>{release.release}</h4>
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
+    const [nweet, setNweet] = useState("");
+  const [nweets, setNweets] = useState([]);
+  const getNweets = async () => {
+    const dbNweets = await dbService.collection("nweets").get();
+    dbNweets.forEach((document) => {
+      const nweetObject = {
+        ...document.data(),
+        id: document.id,
+      };
+      setNweets((prev) => [nweetObject, ...prev]);
+    });
+  };
+  useEffect(() => {
+    getNweets();
+  }, []);
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await dbService.collection("nweets").add({
+      nweet,
+      createdAt: Date.now(),
+    });
+    setNweet("");
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNweet(value);
+  };
+  console.log(nweets);
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={nweet}
+          onChange={onChange}
+          type="text"
+          placeholder="리뷰를 남겨주세요!"
+          maxLength={120}
+        />
+        <input type="submit" value="저장" />
+      </form>
+      <div>
+        {nweets.map((nweet) => (
+          <div key={nweet.id}>
+            <h4>{nweet.nweet}</h4>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 export default Release;
