04a625117c30dcaac6a17d0acc7afc4238cef5d9
minha62 <71062967+minha62@users.noreply.github.com>
2020-12-13 14:17:27 +0900

    Create Community.js

---
+++ src/routes/Community.js
@@ -0,0 +1,57 @@
+import { dbService } from "fbase";
+import React, { useEffect, useState } from "react";
+
+const Community = () => {
+    const [nweet, setNweet] = useState("");
+  const [nweets, setNweets] = useState([]);
+  const getNweets = async () => {
+    const dbNweets = await dbService.collection("Community").get();
+    dbNweets.forEach((document) => {
+      const nweetObject = {
+        ...document.data(),
+        id: document.id,
+      };
+      setNweets((prev) => [nweetObject, ...prev]);
+    });
+  };
+  useEffect(() => {
+    getNweets();
+  }, []);
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await dbService.collection("Community").add({
+      text: nweet,
+      createdAt: Date.now(),
+    });
+    setNweet("");
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNweet(value);
+  };
+  console.log(nweets);
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={nweet}
+          onChange={onChange}
+          type="text"
+          placeholder="새 글을 작성해주세요!"
+          maxLength={300}
+        />
+        <input type="submit" value="저장" />
+      </form>
+      <div>
+        {nweets.map((nweet) => (
+          <div key={nweet.id}>
+            <h4>{nweet.nweet}</h4>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+export default Community;
