9af51dad656f2a332d09d5c3efda21a9e455c6fa
minha62 <71062967+minha62@users.noreply.github.com>
2020-12-16 15:16:38 +0900

    Update Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,22 +4,10 @@ import {Link} from "react-router-dom"
 const Navigation = () => <nav>
     <ul>
         <li>
-            <Link to="/">Search</Link>
-        </li>
-        <li>
-            <Link to="/Release">Release</Link>
-        </li>
-        <li>
-            <Link to="/Roommates">Roommates</Link>
+            <Link to="/">Main</Link>
         </li>
         <li>
             <Link to="/Community">Community</Link>
-        </li>
-        <li>
-            <Link to="/Notice">Notice</Link>
-        </li>
-        <li>
-            <Link to="/MyProfile">My Profile</Link>
         </li>
         <li>
             <Link to="/LogOut">Log Out</Link>
